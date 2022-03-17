273ff8be3bd859e879cae522ac1fb27fe39d7905
Andrey Shibaev <andreishibaev@yandex.ru>
2022-03-17 11:03:39 +0700

    change profile information

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from account.views import ChangePasswordView
+
 
 app_name = 'account'
 
@@ -8,4 +10,5 @@ urlpatterns = [
     path('login/', views.login_account, name='loginuser'),
     path('logout/', views.logoutuser, name='logoutuser'),
     path('profile/', views.profileuser, name='profileuser'),
+    path('password-change/', ChangePasswordView.as_view(), name='password_change'),
 ]
--- account/views.py
+++ account/views.py
@@ -4,6 +4,13 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.decorators import login_required
+from .forms import ProfileForm
+from django.contrib import messages
+
+from django.urls import reverse_lazy
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.messages.views import SuccessMessageMixin
+
 
 
 def register_account(request):
@@ -12,16 +19,17 @@ def register_account(request):
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
+
                 user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
                 user.save()
                 login(request, user)
                 return redirect('account:profileuser')
             except IntegrityError:
                 return render(request, 'account/register_account.html',
-                              {'form': UserCreationForm(), 'error': 'Пользователь уже существует'})
+                              {'form': UserCreationForm(), 'error': 'User already exists'})
         else:
             return render(request, 'account/register_account.html',
-                          {'form': UserCreationForm(), 'error': 'Пароли не совпадают'})
+                          {'form': UserCreationForm(), 'error': 'Passwords do not match'})
 
 
 
@@ -29,11 +37,13 @@ def login_account(request):
     if request.method == 'GET':
         return render(request, 'account/login.html', {'form': AuthenticationForm()})
     else:
-        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
         if user is None:
             return render(request, 'account/login.html',
                           {'form': AuthenticationForm(),
-                           'error': 'Пользователь или пароль не определены. Пройди регистрацию'})
+                           'error': 'User or password not defined. Register'})
         else:
             login(request, user)
             return redirect('account:profileuser')
@@ -45,6 +55,24 @@ def logoutuser(request):
         logout(request)
         return redirect('homeapp:homepage')
 
+
+ 
 @login_required
 def profileuser(request):
-    return render(request, 'account/profileuser.html')
+    if request.method == 'POST':
+        profile_form = ProfileForm(request.POST, instance=request.user)
+        if profile_form.is_valid():
+            profile_form.save()
+            messages.success(request, 'Your profile has been updated!')
+            return redirect('account:profileuser')
+        else:
+            return render(request, 'account/profileuser.html', {'profile_form': profile_form})    
+    else:
+        profile_form = ProfileForm()
+        return render(request, 'account/profileuser.html', {'profile_form': profile_form})
+
+
+class ChangePasswordView(SuccessMessageMixin, PasswordChangeView):
+    template_name = 'account/change_password.html'
+    success_message = "Successfully Changed Your Password"
+    success_url = reverse_lazy('account:profileuser')        
